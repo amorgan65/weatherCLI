a1995692f7f44cae6a8fbf26de6f2db84794246d
Alec Morgan <alec.morgan@berkeley.edu>
2022-07-02 21:01:43 -0700

    Updated method for retrieving JSON data

--- .weatherCLI.sh
+++ .weatherCLI.sh
@@ -6,12 +6,17 @@ function temp() {
   #longitude lon float from second line
   LON=$(./whereami | sed -n '2 p' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 
-  ID=""  #<--- API ID Key in quotes here.
+  KEY="0f17b4981ed7156a5f38d31a86bbe66a"  #<--- API Key in quotes here.
 
-  URL="https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&units=imperial&appid=${ID}"
+  URL="https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&units=imperial&appid=${KEY}"
 
-  response=$(curl -s ${URL} | jq '.main.temp')
+  curl -s --create-dirs -o weatherData.json --output-dir /usr/local/bin/weatherData ${URL} -H "Accept: application/json"
 
-  echo "It is ${response} degrees."
-}
+  file_path="/usr/local/bin/weatherData/weatherData.json"
 
+  temp=$(jq '.main.temp' ${file_path})
+  location=$(jq '.sys.name' ${file_path})
+
+  echo "It is ${temp} degrees in ${location}."
+  
+  rm ${file_path} 
